65e72e22a26cacc1ed312b2e058de2defebe3525
supertorpe <supertorpe@hotmail.com>
2023-03-11 19:37:43 +0100

    fix icon binding

--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ import iconHome from '@ionic/core/dist/ionic/svg/home.svg';
 import iconMenu from '@ionic/core/dist/ionic/svg/menu.svg';
 
 const ICON_MAP = ['home.svg', iconHome, 'menu.svg', iconMenu];
-
-document.querySelectorAll('ion-icon').forEach((el) => {
-  const dataSrc = el.getAttribute('data-src');
-  if (dataSrc != null) el.src = ICON_MAP[ICON_MAP.indexOf(dataSrc)+1];
-})
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('ion-icon').forEach((el) => {
+    const dataSrc = el.getAttribute('data-src');
+    if (dataSrc != null) el.src = ICON_MAP[ICON_MAP.indexOf(dataSrc) + 1];
+  });
+});
